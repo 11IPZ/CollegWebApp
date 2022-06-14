15314a1bbe2a2568faaa607dd1ab7b2fc0ed4024
Olegqqqmarkelov <62740940+Olegqqqmarkelov@users.noreply.github.com>
2022-06-14 18:12:50 +0300

    Додав GroupController

---
+++ CollegWeb/CollegWeb.Domain/ViewModels/Group/CreateGroupViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollegWeb.Domain.ViewModels.Group
+{
+    public class CreateGroupViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
--- CollegWeb/CollegWeb/Controllers/GroupController.cs
+++ CollegWeb/CollegWeb/Controllers/GroupController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CollegWeb.DAL.Interfaces;
+using CollegWeb.Domain.Models;
+using CollegWeb.Domain.Response;
+using CollegWeb.Domain.ViewModels.Group;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CollegWeb.Controllers
 {
     public class GroupController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<UserApp> _userManager;
+        private readonly IGroupRepository _group;
+
+        public GroupController(IGroupRepository group, UserManager<UserApp> userManager)
         {
-            return View();
+            _group = group;
+            _userManager = userManager;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            string id = _userManager.GetUserId(User);
+            BaseResponse<ICollection<Group>> baseResponseGroups = await _group.GetGroupsByUserId(id);
+            var groups = (List<Group>)baseResponseGroups.Data;
+
+            return View(groups);
+        }
+
+        [HttpGet]
+        public IActionResult Create() => View();
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateGroupViewModel model)
+        {
+            
+        }
+
     }
 }
